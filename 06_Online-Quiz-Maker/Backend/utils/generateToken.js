59c6a8fa577ab7da619d88e3e645ef4d538c1c37
Pushpak0000 <bca2005wala@gmail.com>
2025-12-30 13:23:07 +0530

    some comment

--- 06_Online-Quiz-Maker/Backend/utils/generateToken.js
+++ 06_Online-Quiz-Maker/Backend/utils/generateToken.js
@@ -9,11 +9,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-<<<<<<< HEAD
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-=======
->>>>>>> 2627244c9b7167b082e6c49c4abe9fca988ece30
     secure: true,        // ✅ REQUIRED for HTTPS
     sameSite: "None",    // ✅ REQUIRED for cross-site cookies
     maxAge: 7 * 24 * 60 * 60 * 1000,
